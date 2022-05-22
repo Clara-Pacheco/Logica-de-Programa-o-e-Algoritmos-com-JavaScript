7de7413eeb92c961b3112e836a0906f69914e583
Clara-Pacheco <clarapachecodev@gmail.com>
2022-05-22 13:15:39 -0300

    Fix minor bugs- change dinamically size and position of some elements

--- cap02/js/resp2_b.js
+++ cap02/js/resp2_b.js
@@ -2,6 +2,7 @@ function calcularValor() {
   const preco15min = document.querySelector('#preco15min').value
   const tempoUso = document.querySelector('#tempoUso').value
   const resp = document.querySelector('h3')
+  const button = document.querySelector('#botao')
 
   const qtd15Min = Math.ceil(tempoUso / 15) 
   const precoTotal = qtd15Min * preco15min
@@ -9,7 +10,8 @@ function calcularValor() {
   console.log(precoTotal)
 
   resp.innerHTML= `Valor a Pagar R$ ${precoTotal.toFixed(2)}`
-
+  resp.style.marginTop = '0'
+  botao.style.padding='15px'
   const imagem = new Image(466,310)
   imagem.src='https://st2.depositphotos.com/1006832/6429/i/600/depositphotos_64297401-stock-photo-people-playing-games-in-internet.jpg'
   resp.appendChild(imagem)
